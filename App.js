61cf439759dc2197efc2e93b872d53487ffc0474
Pacool84 <flopez_1384@hotmail.com>
2023-01-09 20:28:32 -0600

    [ADD] Basic changes to set up dev environment

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello my friend REACT Native</Text>
+      <Text> 🤯 Hello my friend REACT Native 🤖</Text>
       <StatusBar style="auto" />
     </View>
   );
