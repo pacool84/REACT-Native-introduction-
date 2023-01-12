6e66e4e39bf5aab8b32af9ca84a93a11e4f6dfc2
Pacool84 <flopez_1384@hotmail.com>
2023-01-11 22:12:42 -0600

    [ADD] Basic component to the project

---
+++ components/LoginForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function LoginForm() {
+  return (
+    <View>
+      <Text>Hello My Friend Component LoginForm</Text>
+    </View>
+  );
+}
+
+export default LoginForm;
