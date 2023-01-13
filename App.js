0bd0fcf1cc855bf710b818cc087d679c6dff4f1e
Pacool84 <flopez_1384@hotmail.com>
2023-01-12 22:52:10 -0600

    [REMOVE] Login component from the app file

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import LoginForm from "./components/LoginForm";
 import Saludar from "./components/Saludar";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text> 🤯 Hello my friend REACT Native 🤖</Text>
-      <LoginForm></LoginForm>
-      <Saludar></Saludar>
+      <Saludar />
       <StatusBar style="auto" />
     </View>
   );
